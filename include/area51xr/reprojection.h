9a3ed0265bf9aa789c7063d4d0221a5918c65de0
Joshua <85251908+whitehatws@users.noreply.github.com>
2026-08-20 19:38:36 -0400

    Define depth reprojection math

---
+++ include/area51xr/reprojection.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "area51xr/depth_mesh.h"
+
+#include <optional>
+
+namespace area51xr {
+
+struct PixelCoordinate {
+    float x{};
+    float y{};
+};
+
+std::optional<PixelCoordinate> project_world_point(
+    Vec3 world_point,
+    Vec3 eye_position,
+    const CameraIntrinsics& camera,
+    std::uint32_t width,
+    std::uint32_t height) noexcept;
+
+std::optional<NormalizedAim> reproject_world_point_uv(
+    Vec3 world_point,
+    Vec3 eye_position,
+    const CameraIntrinsics& camera,
+    std::uint32_t width,
+    std::uint32_t height) noexcept;
+
+} // namespace area51xr
