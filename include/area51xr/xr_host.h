1c8006b3167ae85c18d9bac9c8e6de66539bc01a
Joshua <85251908+whitehatws@users.noreply.github.com>
2026-08-20 19:16:35 -0400

    Define XR host loop

---
+++ include/area51xr/xr_host.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "area51xr/bridge_host.h"
+#include "area51xr/xr_aim.h"
+#include "area51xr/xr_runtime.h"
+
+namespace area51xr {
+
+struct HostTickResult {
+    bool runtime_ok{};
+    bool session_running{};
+    bool aim_valid{};
+    NormalizedAim aim{};
+    bool trigger_down{};
+    FrameStatus frame{};
+};
+
+class XrHost {
+public:
+    XrHost(XrRuntime& runtime, MameSharedState& shared, AimPlane plane = {});
+
+    bool initialize();
+    HostTickResult tick();
+    void shutdown();
+
+private:
+    XrRuntime& runtime_;
+    MameSharedState& shared_;
+    AimPlane plane_{};
+    NormalizedAim last_aim_{0.5f, 0.5f};
+    bool initialized_{};
+};
+
+} // namespace area51xr
