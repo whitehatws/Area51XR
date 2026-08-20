8ba0c9459dce63eca50497294d8e65ea312e44fa
Joshua <85251908+whitehatws@users.noreply.github.com>
2026-08-20 19:18:58 -0400

    Define Windows OpenXR runtime

---
+++ include/area51xr/openxr_runtime.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "area51xr/xr_runtime.h"
+
+#include <memory>
+
+namespace area51xr {
+
+class OpenXrRuntime final : public XrRuntime {
+public:
+    OpenXrRuntime();
+    ~OpenXrRuntime() override;
+
+    OpenXrRuntime(const OpenXrRuntime&) = delete;
+    OpenXrRuntime& operator=(const OpenXrRuntime&) = delete;
+
+    bool initialize() override;
+    bool poll(XrInputState& state) override;
+    void shutdown() override;
+
+    [[nodiscard]] const char* last_error() const noexcept;
+
+private:
+    struct Impl;
+    std::unique_ptr<Impl> impl_;
+};
+
+} // namespace area51xr
