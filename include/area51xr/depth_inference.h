f0096cf48434274635e7a59ae89373ac85567f3e
Joshua <85251908+whitehatws@users.noreply.github.com>
2026-08-20 20:03:30 -0400

    Add tensor inference backend abstraction

---
+++ include/area51xr/depth_inference.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "area51xr/depth_anything_v2.h"
+#include "area51xr/depth_provider.h"
+
+#include <cstdint>
+#include <span>
+#include <vector>
+
+namespace area51xr {
+
+class TensorInferenceBackend {
+public:
+    virtual ~TensorInferenceBackend() = default;
+
+    virtual bool run(
+        std::span<const float> chw_rgb,
+        std::uint32_t input_width,
+        std::uint32_t input_height,
+        std::vector<float>& output,
+        std::uint32_t& output_width,
+        std::uint32_t& output_height) = 0;
+};
+
+class DepthAnythingV2Provider final : public DepthProvider {
+public:
+    DepthAnythingV2Provider(
+        TensorInferenceBackend& backend,
+        std::uint32_t input_size = 518,
+        RelativeDepthCalibration calibration = {});
+
+    bool estimate(const ColorFrameView& frame, DepthEstimate& output) override;
+
+    [[nodiscard]] const RelativeDepthStats& last_stats() const noexcept;
+
+private:
+    TensorInferenceBackend& backend_;
+    std::uint32_t input_size_{};
+    RelativeDepthCalibration calibration_{};
+    DepthAnythingTensor input_{};
+    std::vector<float> raw_output_;
+    RelativeDepthStats stats_{};
+};
+
+} // namespace area51xr
