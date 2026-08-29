d66a3eaefcaf01c72e70ae48532fb3844c22e9b0
Joshua <85251908+whitehatws@users.noreply.github.com>
2026-08-29 16:40:31 -0400

    Guard Win32 bridge macros

--- mame/area51xr_mame_bridge.h
+++ mame/area51xr_mame_bridge.h
@@ -1,8 +1,12 @@
 #pragma once
 
 #ifdef _WIN32
+#ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
+#endif
+#ifndef NOMINMAX
 #define NOMINMAX
+#endif
 #include <windows.h>
 #ifdef ERROR
 #undef ERROR
