882a3b987d95bd3f5c0bda9add65f29bc7e58f5d
Joshua <85251908+whitehatws@users.noreply.github.com>
2026-08-29 15:32:10 -0400

    Add offscreen gun state

--- include/area51xr/mame_bridge.h
+++ include/area51xr/mame_bridge.h
@@ -12,7 +12,8 @@ struct MameGunState {
     float aim_x{};
     float aim_y{};
     std::uint8_t trigger{};
-    std::uint8_t reserved[7]{};
+    std::uint8_t offscreen{};
+    std::uint8_t reserved[6]{};
 };
 
 struct MameFrameHeader {
